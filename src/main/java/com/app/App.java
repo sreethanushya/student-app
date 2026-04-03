13b9c22084371eb56616ac2869e2b82f935dba9e
sreethanushya <sreethanushya.m2023@vitstudent.ac.in>
2026-04-03 19:37:11 +0530

    changed

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,11 +1,16 @@
 package com.app;
 
 public class App {
+
     public static void main(String[] args) {
         System.out.println("Student Result System Running");
 
         System.out.println("Student Details:");
         System.out.println("Name: Thanu");
+<<<<<<< HEAD
         System.out.println("Marks: 98");
+=======
+        System.out.println("Marks: 96");
+>>>>>>> 0cc280f (changes made again)
     }
 }
