9bc352620bab9ef70571c12e18dab350dfe4558c
sreethanushya <sreethanushya.m2023@vitstudent.ac.in>
2026-04-03 18:28:43 +0530

    inculding java source code

---
+++ src/main/java/com/app/App.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Student Result System Running");
+
+        System.out.println("Student Details:");
+        System.out.println("Name: Thanu");
+        System.out.println("Marks: 95");
+    }
+}
